46ab70f9713eaf81025d351c3394d6c98c04ff2b
Sondre T. Johannessen <sondre@vimond.com>
2017-05-17 22:21:28 +0200

    Added redux stuff.

--- ReactExample/src/index.js
+++ ReactExample/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import App from "./components/App";
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import reducer from './reducer.js';
 
 ReactDOM.render(
   <App/>,
